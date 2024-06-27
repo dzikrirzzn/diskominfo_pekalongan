f1808e62f039b5186555bcb80b292e395d93a402
brvlyd <bravelydirgayuska@gmail.com>
2024-06-27 09:53:42 +0700

    sv3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,59 +42,56 @@
                 </div>
             </div>
         </div>
+        <!-- Carousel -->
         <div class="p-16">
-            <div class="max-w-4xl mx-auto relative" x-data="{
-                activeSlide: 1,
-                slides: [
-                    { id: 1, title: 'Hello 1', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, magni a veniam minus nemo expedita eos veritatis vitae voluptate porro. Quo velit eius ea ipsam? Temporibus placeat dolore quisquam quod.'},
-                    { id: 2, title: 'Hello 2', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, magni a veniam minus nemo expedita Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, molestias.'},
-                    { id: 3, title: 'Hello 3', body: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, molestias.'},
-                    { id: 4, title: 'Hello 4', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, magni a veniam minus Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, molestias.'},
-                    { id: 5, title: 'Hello 5', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, molestias.'}
-                ],
-                loop(){
-                    setInterval(() => {
-                        this.activeSlide = this.activeSlide === 5 ? 1 : this.activeSlide + 1
-                    }, 2000)
-                }
-            }" x-init="loop">
-                <!-- Carousel -->
-                <template x-for="slide in slides" :key="slide.id">
-                    <div x-show="activeSlide === slide.id" class="p-24 h-80 flex items-center bg-slate-500 text-white rounded-lg">
-                        <div>
-                            <h2 class="font-bold text-2xl" x-text="slide.title"></h2>
-                            <p x-text="slide.body" class="text-base"></p>
-                        </div>
-                    </div>
-                </template>
-                <!-- Back/Next Buttons -->
-                <div class="absolute inset-0 flex">
-                    <div class="flex items-center justify-end w-1/2">
-                        <button x-on:click="activeSlide = activeSlide === 1 ? slides.length : activeSlide - 1" class="bg-slate-100 text-slate-500 hover:bg-blue-500 hover:text-white font-bold rounded-full w-12 h-12 shadow flex justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="flex items-center justify-start w-1/2">
-                        <button x-on:click="activeSlide = activeSlide === slides.length ? 1 : activeSlide + 1" class="bg-slate-100 text-slate-500 hover:bg-blue-500 hover:text-white font-bold rounded-full w-12 h-12 shadow flex justify-center items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <!-- Pagination Buttons -->
-                <div class="absolute w-full flex items-center justify-center px-4">
-                    <template x-for="slide in slides" :key="slide.id">
-                        <button class="flex w-4 h-2 mt-4 mx-2 mb-2 rounded-full overflow-hidden transition colors duration-200 ease-out hover:bg-slate-600 hover:shadow-lg" :class="{
-                        'bg-blue-600' : activeSlide === slide.id,
-                        'bg-slate-300' : activeSlide !== slide.id,
-                        }" x-on:click="activeSlide = slide.id"></button>
-                    </template>
+    <div class="max-w-4xl mx-auto relative" x-data="{
+        activeSlide: 1,
+        slides: [
+            { id: 1, title: 'Hello 1', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, magni a veniam minus nemo expedita eos veritatis vitae voluptate porro. Quo velit eius ea ipsam? Temporibus placeat dolore quisquam quod.'},
+            { id: 2, title: 'Hello 2', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, magni a veniam minus nemo expedita Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, molestias.'},
+            { id: 3, title: 'Hello 3', body: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, molestias.'},
+            { id: 4, title: 'Hello 4', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, magni a veniam minus Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, molestias.'},
+            { id: 5, title: 'Hello 5', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+        ],
+        loop(){
+            setInterval(() => {
+                this.activeSlide = this.activeSlide === 5 ? 1 : this.activeSlide + 1
+            }, 5000)
+        }
+    }" x-init="loop">
+        <!-- Data Loop -->
+        <template x-for="slide in slides" :key="slide.id">
+            <div x-show="activeSlide === slide.id" class="p-24 h-80 flex items-center bg-slate-500 text-white rounded-lg">
+                <div>
+                    <h2 class="font-bold text-2xl" x-text="slide.title"></h2>
+                    <p x-text="slide.body" class="text-base"></p>
                 </div>
             </div>
+        </template>
+        <!-- Back/Next Buttons -->
+            <div class="absolute inset-0 flex justify-between">
+            <button x-on:click="activeSlide = activeSlide === 1 ? slides.length : activeSlide - 1" class="bg-slate-100 text-slate-500 hover:bg-blue-500 hover:text-white font-bold rounded-full w-12 h-12 shadow flex justify-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
+                </button>
+                <button x-on:click="activeSlide = activeSlide === slides.length ? 1 : activeSlide + 1" class="bg-slate-100 text-slate-500 hover:bg-blue-500 hover:text-white font-bold rounded-full w-12 h-12 shadow flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>
+            </button>
+            </div>
+        <!-- Pagination Buttons -->
+            <div class="absolute inset-y-0 flex items-center justify-between px-4 w-full">
+            <template x-for="slide in slides" :key="slide.id">
+                <button class="flex w-4 h-2 mx-2 rounded-full overflow-hidden transition colors duration-200 ease-out hover:bg-slate-600 hover:shadow-lg" :class="{
+                'bg-blue-600' : activeSlide === slide.id,
+                'bg-slate-300' : activeSlide !== slide.id,
+                }" x-on:click="activeSlide = slide.id"></button>
+            </template>
+            </div>
         </div>
+    </div>
         <!-- Add the map div here -->
         <div id="map" style="height: 400px; margin-top: 2rem;"></div>
     </main>
