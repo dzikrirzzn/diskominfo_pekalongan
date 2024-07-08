2005f568a012c81673503eae963dc7ebef20353e
NajlaFairuzAzzahra <najlafairuzazzahra12@gmail.com>
2024-07-08 08:58:33 +0700

    ll

--- resources/views/admin_berita.blade.php
+++ resources/views/admin_berita.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload Pengumuman</title>
+    <title>Upload Berita</title>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -14,7 +14,7 @@
     <x-app-layout>
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Upload Pengumuman') }}
+                {{ __('Upload Berita') }}
             </h2>
         </x-slot>
 
@@ -33,44 +33,50 @@
                             {{ session('error') }}
                         </div>
                         @endif
-                        <form method="POST" action="{{ route('pengumuman.store') }}" enctype="multipart/form-data">
+                        <form method="POST" action="{{ route('berita.store') }}" enctype="multipart/form-data">
                             @csrf
                             <div class="mb-4">
-                                <label for="judul" class="block text-gray-700 text-sm font-bold mb-2">Judul:</label>
-                                <input type="text" id="judul" name="judul"
+                                <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title:</label>
+                                <input type="text" id="title" name="title"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     required>
                             </div>
                             <div class="mb-4">
-                                <label for="tanggal" class="block text-gray-700 text-sm font-bold mb-2">Tanggal:</label>
-                                <input type="text" id="tanggal" name="tanggal"
-                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline datepicker"
-                                    required>
-                            </div>
-                            <div class="mb-4">
-                                <label for="penulis" class="block text-gray-700 text-sm font-bold mb-2">Penulis:</label>
-                                <input type="text" id="penulis" name="penulis"
+                                <label for="subtitle" class="block text-gray-700 text-sm font-bold mb-2">Subtitle:</label>
+                                <input type="text" id="subtitle" name="subtitle"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     required>
                             </div>
                             <div class="mb-4">
-                                <label for="isi_pengumuman" class="block text-gray-700 text-sm font-bold mb-2">Isi
-                                    Pengumuman:</label>
-                                <textarea id="isi_pengumuman" name="isi_pengumuman" rows="5"
+                                <label for="content" class="block text-gray-700 text-sm font-bold mb-2">Content:</label>
+                                <textarea id="content" name="content" rows="5"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     required></textarea>
                             </div>
                             <div class="mb-4">
-                                <label for="gambar_pengumuman" class="block text-gray-700 text-sm font-bold mb-2">Gambar
-                                    Pengumuman:</label>
-                                <input type="file" id="gambar_pengumuman" name="gambar_pengumuman"
+                                <label for="author" class="block text-gray-700 text-sm font-bold mb-2">Author:</label>
+                                <input type="text" id="author" name="author"
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    required>
+                            </div>
+                            <div class="mb-4">
+                                <label for="date" class="block text-gray-700 text-sm font-bold mb-2">Date:</label>
+                                <input type="text" id="date" name="date"
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline datepicker"
+                                    required>
+                            </div>
+                            <div class="mb-4">
+                                <label for="image" class="block text-gray-700 text-sm font-bold mb-2">Image:</label>
+                                <input type="file" id="image" name="image"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                             </div>
                             <div class="mb-4">
-                                <label for="link_pdf_pengumuman" class="block text-gray-700 text-sm font-bold mb-2">Link
-                                    PDF Pengumuman:</label>
-                                <input type="file" id="link_pdf_pengumuman" name="link_pdf_pengumuman"
-                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                                <label for="type" class="block text-gray-700 text-sm font-bold mb-2">Type:</label>
+                                <select id="type" name="type"
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                                    <option value="kota">Kota</option>
+                                    <option value="lainnya">Lainnya</option>
+                                </select>
                             </div>
                             <div class="mb-4">
                                 <button type="submit"
